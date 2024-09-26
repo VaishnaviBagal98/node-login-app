23601c81349e891738a9d5ed71b464253e166507
Vaishnavi <vaishnavi.bagal@outlook.com>
2024-09-26 21:30:40 +0530

    Initial commit

--- login-system/src/Signup.jsx
+++ login-system/src/Signup.jsx
@@ -1,17 +1,20 @@
 import { useState } from "react";
 import {Link } from "react-router-dom";
 import axios from 'axios';
+import { useNavigate } from "react-router-dom";
 
 function Signup(){
 
     const[name,setName] = useState()
     const[email,setEmail] = useState()
     const[password,setPassword] = useState()
+    const navigate = useNavigate();
 
     const handleSubmit = (e)=>{
         e.preventDefault()
-        axios.post('',{name,email,password})
+        axios.post('http://localhost:3001/register',{name,email,password})
         .then(result => console.log(result))
+        navigate('/login')
         .catch(err=> console.log(err))
     }
       
@@ -19,7 +22,7 @@ function Signup(){
         <div className="d-flex justify-content-center align-items-center bg-secondary vh-100">
             <div className="bg-white p-3 rounded w-25">
             <h2>Register</h2>
-            <form onsubmit={handelSubmit}>
+            <form onSubmit={handleSubmit}>
             <div className="mb-3">
                 <label htmlFor="email">
                     <strong>Name</strong>
